38dfa0ccd9e9c0d473e83c6238c27090f801a480
Raya Straus <raya@cloudinary.com>
2018-06-25 14:24:06 +0300

    adding ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './Navigation.js'
+import { Route, Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img className="left" src="https://res.cloudinary.com/cloudinary/image/upload/c_scale,w_200/v1/logo/for_white_bg/cloudinary_logo_for_white_bg.png" />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route exact={true} path={'/'} component={Navigation}/>
       </div>
     );
   }
